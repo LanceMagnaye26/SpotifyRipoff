9b0e7f31221088378e3174607e8f5d84e7226288
Siming Yang <Yangsiming1999@gmail.com>
2019-01-31 11:20:22 -0800

    added node with search

---
+++ index.js
@@ -0,0 +1,29 @@
+// constants
+
+const exp = require("express");
+const port = process.env.PORT || 10000;
+const path = require("path");
+const bodyParser = require("body-parser");
+
+var pF = path.resolve(__dirname, "public");
+var app = exp();
+
+const server = require("http").createServer(app);
+
+app.use("/scripts", exp.static("build"));
+app.use("/css", exp.static("style"));
+app.use("/pages",exp.static("public"));
+app.use("/dependencies",exp.static("src"))
+
+app.get("/menufooter", function(req, resp){
+    resp.sendFile(pF+"/menufooter.html")
+});
+
+server.listen(10000, function(err){
+    if(err){
+        console.log(err);
+        return false;
+    }
+    
+    console.log(port+" is running");
+});
